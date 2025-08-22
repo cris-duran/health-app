c3ea976dd1648f2b076f8bffa6beb6688e4b3f59
cris-duran <creature.secret@gmail.com>
2025-08-22 11:48:59 -0400

    feat: crear tabla de usuarios

---
+++ db/config/config.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB_TEST,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl:
+        process.env.POSTGRES_SSLMODE === "require"
+          ? { require: true, rejectUnauthorized: false }
+          : undefined,
+    },
+  },
+};
