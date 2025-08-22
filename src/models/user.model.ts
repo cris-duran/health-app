a952ea03143808f0ca76364109eab81a8ad61810
cris-duran <creature.secret@gmail.com>
2025-08-22 11:59:09 -0400

    feat: se agrega el modelo user

---
+++ src/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  age: number; // years
+  height: number; // cm
+  gender: boolean; // true: male, false: female
+}
